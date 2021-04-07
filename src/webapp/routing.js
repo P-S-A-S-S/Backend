8e201f06d012c64a684b3209f5ade449502d2d68
BlackkSight <gtracer.jsx@gmail.com>
2021-04-07 19:32:29 +0200

    Adding certs

--- src/webapp/routing.js
+++ src/webapp/routing.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
+
 function startBackend(){
     const app = express();
     const port = 5000;
-    const public = express.static('public')
+    const public = express.static('public');
+    const options = {
+        key: fs.readFileSync('./certs/server.key'),
+        cert: fs.readFileSync('./certs/server.crt'),
+    };
+    const server = https.createServer(options, app).listen(port, () => {
+        console.log("Servidor actiu en el port", port);
+    });
     app.get('/', public);
+    app.get('/consolelog', (req, res)=>{
+        console.log("Esto se ejecuta en la consola del servidor");
+        res.send("Accion realizada.");
+    });
     app.get('*', (req, res)=>{
         res.send("Error 404: Bro esta pagina no existe XD")
-    });
-    app.listen(port, () => {
-        console.log("Servidor actiu en el port", port);
     });
 };
 
