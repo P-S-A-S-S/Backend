83ee8d69f97ddcaf6a78cdbc7775fd1812495450
a14pauagurad <a14pauagurad@gmail.com>
2021-04-09 19:30:30 +0200

    implementación sockets a index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const api = require('./src/webapp/routing');
-
+const srvSock = require('./src/clientback/server-manager')
 api.startBackend();
+srvSock.startSockets();
--- src/clientback/server-manager.js
+++ src/clientback/server-manager.js
@@ -1,43 +1,50 @@
 const net = require("net");
 const fs = require ("fs");
-const server = net.createServer();
-const port = 1232;
-const host = "0.0.0.0";
-const tout = 3000 //ms para timeout
-let sockets = [];
 
-server.listen(port, host, () => {
-	console.log(`TCP server listening on ${host}:${port}`);
-});
 
-server.on("connection", (socket) => {
-	var clientAddress = `${socket.remoteAddress}:${socket.remotePort}`;
-	console.log(`new client connected: ${clientAddress}`);
-	sockets.push(socket);
-	console.log(sockets.length);
-	socket.on("data", (data) => {
-    	console.log(`${clientAddress}: ${data}`); //output mensaje cliente
-    	console.log(JSON.stringify(data)); //strin a Json
+function startSockets() {
+	const server = net.createServer();
+	const port = 1234;
+	const host = "0.0.0.0";
+	const tout = 3000 //ms para timeout
+	let sockets = [];
+
+
+	server.listen(port, host, () => {
+		console.log(`TCP server listening on ${host}:${port}`);
 	});
-	//Timeout
-	socket.setTimeout(tout);
-	socket.on('timeout', () => {
-  		console.log('socket timeout');
-		socket.end();
-	});
-	//salida
-	socket.on('close', (data) => { 
-        const index = sockets.findIndex( (o) => { 
-            return (o.remoteAddress===socket.remoteAddress) && (o.remotePort === socket.remotePort); 
-        }); 
-        if (index !== -1) sockets.splice(index, 1); 
-		sockets.forEach((sock) => { 
-			sock.write(`${clientAddress} disconnected\n`); 
+
+	server.on("connection", (socket) => {
+		var clientAddress = `${socket.remoteAddress}:${socket.remotePort}`;
+		console.log(`new client connected: ${clientAddress}`);
+		sockets.push(socket);
+		console.log(sockets.length);
+		socket.on("data", (data) => {
+	    	console.log(`${clientAddress}: ${data}`); //output mensaje cliente
+	    	console.log(JSON.stringify(`${data}`)); //strin a Json
+		});
+		//Timeout
+		socket.setTimeout(tout);
+		socket.on('timeout', () => {
+	  		console.log('socket timeout');
+			socket.end();
+		});
+		//salida
+		socket.on('close', (data) => { 
+	        const index = sockets.findIndex( (o) => { 
+	            return (o.remoteAddress===socket.remoteAddress) && (o.remotePort === socket.remotePort); 
+	        }); 
+	        if (index !== -1) sockets.splice(index, 1); 
+			sockets.forEach((sock) => { 
+				sock.write(`${clientAddress} disconnected\n`); 
+			}); 
+			console.log(`connection closed: ${clientAddress}`); 
+	    }); 
+		// Gestor d'errors 
+		socket.on("error", (err) => { 
+			console.log(`Error occurred in ${clientAddress}: ${err.message}`); 
 		}); 
-		console.log(`connection closed: ${clientAddress}`); 
-    }); 
-	// Gestor d'errors 
-	socket.on("error", (err) => { 
-		console.log(`Error occurred in ${clientAddress}: ${err.message}`); 
-	}); 
-});
+	});
+};
+
+exports.startSockets = startSockets;
