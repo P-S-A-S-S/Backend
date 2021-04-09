8ed3a6e24297847c1ec94f5857c21f4034742c4c
a14pauagurad <a14pauagurad@gmail.com>
2021-04-09 18:28:59 +0200

    update server

--- src/clientback/server-manager.js
+++ src/clientback/server-manager.js
@@ -1,8 +1,9 @@
 const net = require("net");
+const fs = require ("fs");
 const server = net.createServer();
 const port = 1232;
 const host = "0.0.0.0";
-
+const tout = 3000 //ms para timeout
 let sockets = [];
 
 server.listen(port, host, () => {
@@ -15,9 +16,16 @@ server.on("connection", (socket) => {
 	sockets.push(socket);
 	console.log(sockets.length);
 	socket.on("data", (data) => {
-    	console.log(`${clientAddress}: ${data}`);
+    	console.log(`${clientAddress}: ${data}`); //output mensaje cliente
+    	console.log(JSON.stringify(data)); //strin a Json
 	});
-//salida
+	//Timeout
+	socket.setTimeout(tout);
+	socket.on('timeout', () => {
+  		console.log('socket timeout');
+		socket.end();
+	});
+	//salida
 	socket.on('close', (data) => { 
         const index = sockets.findIndex( (o) => { 
             return (o.remoteAddress===socket.remoteAddress) && (o.remotePort === socket.remotePort); 
@@ -28,17 +36,6 @@ server.on("connection", (socket) => {
 		}); 
 		console.log(`connection closed: ${clientAddress}`); 
     }); 
-	// gestiona la sortida de clients 
-//	socket.on('close', (data) => { 
-//		letindex = sockets.findIndex((o) => { 
-//			o.remoteAddress === socket.remoteAddress && o.remotePort === socket.remotePort; 
-//	    }) 
-//		if (index !== -1) sockets.splice(index, 1); 
-//			sockets.forEach((sock) => { 
-//				sock.write(`${clientAddress} disconnected\n`); 
-//	        }); 
-//		console.log(`connection closed: ${clientAddress}`);
-//	});
 	// Gestor d'errors 
 	socket.on("error", (err) => { 
 		console.log(`Error occurred in ${clientAddress}: ${err.message}`); 
