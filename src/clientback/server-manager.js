f25aeb67ce8079c233be34372cd83067e264d289
a14pauagurad <a14pauagurad@gmail.com>
2021-04-09 18:26:44 +0200

    update server

--- src/clientback/server-manager.js
+++ src/clientback/server-manager.js
@@ -1,12 +1,46 @@
-var net = require('net');
-var server = net.createServer(function(socket) {
-        console.log(socket.remoteAddress)
-        socket.write('Echo server\r\n');
-//muestra por consola el texto entrante
-        socket.on('data', function(data) {
-                console.log('Received: ' + data);
-        });
-        socket.pipe(socket);
+const net = require("net");
+const server = net.createServer();
+const port = 1232;
+const host = "0.0.0.0";
+
+let sockets = [];
+
+server.listen(port, host, () => {
+	console.log(`TCP server listening on ${host}:${port}`);
 });
 
-server.listen(1337, '0.0.0.0');
+server.on("connection", (socket) => {
+	var clientAddress = `${socket.remoteAddress}:${socket.remotePort}`;
+	console.log(`new client connected: ${clientAddress}`);
+	sockets.push(socket);
+	console.log(sockets.length);
+	socket.on("data", (data) => {
+    	console.log(`${clientAddress}: ${data}`);
+	});
+//salida
+	socket.on('close', (data) => { 
+        const index = sockets.findIndex( (o) => { 
+            return (o.remoteAddress===socket.remoteAddress) && (o.remotePort === socket.remotePort); 
+        }); 
+        if (index !== -1) sockets.splice(index, 1); 
+		sockets.forEach((sock) => { 
+			sock.write(`${clientAddress} disconnected\n`); 
+		}); 
+		console.log(`connection closed: ${clientAddress}`); 
+    }); 
+	// gestiona la sortida de clients 
+//	socket.on('close', (data) => { 
+//		letindex = sockets.findIndex((o) => { 
+//			o.remoteAddress === socket.remoteAddress && o.remotePort === socket.remotePort; 
+//	    }) 
+//		if (index !== -1) sockets.splice(index, 1); 
+//			sockets.forEach((sock) => { 
+//				sock.write(`${clientAddress} disconnected\n`); 
+//	        }); 
+//		console.log(`connection closed: ${clientAddress}`);
+//	});
+	// Gestor d'errors 
+	socket.on("error", (err) => { 
+		console.log(`Error occurred in ${clientAddress}: ${err.message}`); 
+	}); 
+});
