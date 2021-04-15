8153c6dc56017d6a047dadc5ba6662b5ff335583
a14pauagurad <a14pauagurad@gmail.com>
2021-04-15 16:33:02 +0200

    db connection and insert

--- src/clientback/server-manager.js
+++ src/clientback/server-manager.js
@@ -1,13 +1,16 @@
 const net = require("net");
 const fs = require ("fs");
-
+const mongoCli = require ('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/' //url a mongo
 
 function startSockets() {
 	const server = net.createServer();
 	const port = 1234;
 	const host = "0.0.0.0";
 	const tout = 3000 //ms para timeout
+	//let secret = []; array secretos para cada cli(?)
 	let sockets = [];
+
 
 
 	server.listen(port, host, () => {
@@ -21,16 +24,27 @@ function startSockets() {
 		console.log(sockets.length);
 		socket.on("data", (data) => {
 	    	console.log(`${clientAddress}: ${data}`); //output mensaje cliente
-	    	console.log(JSON.stringify(`${data}`)); //strin a Json
-		});
+	    	const pData = JSON.parse(data)
+	    	console.log(pData.id); //strin a Json
+	    	mongoCli.connect(url, function(err, db) {
+	  			if (err) throw err;
+	  			var dbo = db.db("data");
+	  			var myobj = {"id": 0, "so": "lin", "lIp": "192.168.207.222", "command": "x", "oPut": "testtesttesttest"}
+	  			dbo.collection("clients").insertOne(myobj, function(err, res) {
+	    			if (err) throw err;
+	    			console.log("1 document inserted");
+	    			db.close();
+	  			});
+			}); 
+	    });
 		//Timeout
-		socket.setTimeout(tout);
-		socket.on('timeout', () => {
-	  		console.log('socket timeout');
-			socket.end();
-		});
+		//socket.setTimeout(tout);
+		//socket.on('timeout', () => {
+	  	//	console.log('socket timeout');
+		//	socket.end();
+		//});
 		//salida
-		socket.on('close', (data) => { 
+		socket.on('close', (data) => {
 	        const index = sockets.findIndex( (o) => { 
 	            return (o.remoteAddress===socket.remoteAddress) && (o.remotePort === socket.remotePort); 
 	        }); 
@@ -48,3 +62,5 @@ function startSockets() {
 };
 
 exports.startSockets = startSockets;
+//var crypto = require("crypto");
+//secret.push(crypto.randomBytes(20).toString('hex'));
